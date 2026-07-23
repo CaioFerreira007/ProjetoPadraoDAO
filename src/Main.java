92d502b5d0b01cf1a20f3c514dcd606268291822
Caio Gustavo <caiogggustavo49@gmail.com>
2026-07-23 18:36:47 -0300

    feat: class Departmeent finished, class Seller inicialized

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
 import entities.Department;
+import entities.Seller;
+
+import java.util.Date;
 
 public class Main {
 
     public static void main(String[] args) {
 
         Department department = new Department(1, "Books");
+        Seller seller = new Seller(3000.00, new Date(),department, "John@gmail.com",287,"John");
+        System.out.println(seller);
         System.out.println(department);
     }
 
